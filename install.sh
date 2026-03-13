0b1966e9ab1338081dfd93537d82695c281462a0
sapirrior <jimmynnit1000@gmail.com>
2026-03-13 11:36:49 +0530

    Add Termux support to install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ VERSION="v0.0.1"
 
 OS_NAME="$(uname -s | tr '[:upper:]' '[:lower:]')"
 ARCH_NAME="$(uname -m)"
+IS_TERMUX=false
+
+if [ -n "$PREFIX" ] && [ -d "$PREFIX/bin" ]; then
+  IS_TERMUX=true
+fi
 
 case "$OS_NAME" in
   linux)
@@ -27,7 +32,7 @@ esac
 BINARY_NAME="${VERSION}-cplus-${PLATFORM}"
 DOWNLOAD_URL="https://github.com/${REPO}/releases/download/${VERSION}/${BINARY_NAME}"
 
-echo "Detected Platform: $OS_NAME ($ARCH_NAME)"
+echo "Detected Platform: $OS_NAME ($ARCH_NAME) $( [ "$IS_TERMUX" = true ] && echo "[Termux Detected]" )"
 echo "Downloading C+ $VERSION ($PLATFORM)..."
 
 if ! curl -L --fail -o cplus "$DOWNLOAD_URL"; then
@@ -37,11 +42,30 @@ fi
 
 chmod +x cplus
 
-echo "Installing to /usr/local/bin/cplus (requires sudo)..."
-if sudo mv cplus /usr/local/bin/cplus; then
-  echo "Successfully installed C+!"
-  echo "You can now run it using: cplus"
+if [ "$IS_TERMUX" = true ]; then
+  INSTALL_DIR="$PREFIX/bin"
+  USE_SUDO=false
 else
-  echo "Installation failed. Try moving the 'cplus' file to your PATH manually."
-  exit 1
+  INSTALL_DIR="/usr/local/bin"
+  USE_SUDO=true
 fi
+
+echo "Installing to $INSTALL_DIR/cplus..."
+
+if [ "$USE_SUDO" = true ]; then
+  if sudo mv cplus "$INSTALL_DIR/cplus"; then
+    echo "Successfully installed C+!"
+  else
+    echo "Installation failed. Try moving the 'cplus' file to your PATH manually."
+    exit 1
+  fi
+else
+  if mv cplus "$INSTALL_DIR/cplus"; then
+    echo "Successfully installed C+!"
+  else
+    echo "Installation failed. Make sure $INSTALL_DIR is writable."
+    exit 1
+  fi
+fi
+
+echo "You can now run it using: cplus"
